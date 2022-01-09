41f530f295884562bfae2aadbaf475bd91add9aa
Remco Leijenaar <remco-leijenaar@live.com>
2022-01-09 19:35:24 +0100

    some minor updates to midi_duration for compact representation

--- midi_duration.py
+++ midi_duration.py
@@ -6,10 +6,12 @@ from pandas.core.frame import DataFrame
 
 MIDI_COMPACT = List[List[Tuple[int, float]]]
 
-FILENAME_F = "F.txt"  # Requires tabbed delimited csv
+if __name__ == "__main__":
+    FILENAME_F = "F.txt"  # Requires tab delimited csv
+    OMIT_REST = True
 
 
-def midi_tones_to_midi_compact(midi_notes: np.ndarray, modulation=0, symbols_per_beat=4) -> MIDI_COMPACT:
+def midi_tones_to_midi_compact(midi_notes: np.ndarray, omit_rest=True, modulation=0, symbols_per_beat=4) -> MIDI_COMPACT:
     symbol_len = midi_notes.shape[0]
     n_channels = midi_notes.shape[1]
 
@@ -27,7 +29,7 @@ def midi_tones_to_midi_compact(midi_notes: np.ndarray, modulation=0, symbols_per
             midi_pitch = voice[idx]
             # Change to different tone
             if midi_pitch != prev_midi:
-                if prev_midi != 0:
+                if not omit_rest or prev_midi != 0:
                     duration = (idx - start_idx) / symbols_per_beat
                     pitch = prev_midi + modulation
                     compact_voice.append((pitch, duration))
@@ -37,14 +39,15 @@ def midi_tones_to_midi_compact(midi_notes: np.ndarray, modulation=0, symbols_per
     return compact
 
 
-def midi_tones_file_to_midi_compact(in_file="F.txt") -> MIDI_COMPACT:
+def midi_tones_file_to_midi_compact(in_file="F.txt", **kwargs) -> MIDI_COMPACT:
     frequencies: DataFrame = pd.read_csv(in_file, sep="\t", header=None)
     frequencies = frequencies.to_numpy()
-    return midi_tones_to_midi_compact(frequencies)
+    return midi_tones_to_midi_compact(frequencies, **kwargs)
 
 
 if __name__ == "__main__":
-    out = midi_tones_file_to_midi_compact(FILENAME_F)
+    out = midi_tones_file_to_midi_compact(FILENAME_F, omit_rest=OMIT_REST)
+
     for channel in out:
         print(len(channel))
     # First 20 notes of the first channel
