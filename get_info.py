8aaf6b21fa5a6460c6375a881ac13ade8b5c1834
JeroenOudeVrielink <jeroenov98@gmail.com>
2022-01-10 12:30:29 +0100

    updating print statements

--- get_info.py
+++ get_info.py
@@ -6,23 +6,23 @@ from pandas import DataFrame
 frequencies: DataFrame = pd.read_csv("F.txt", sep="\t", header=None)
 frequencies = frequencies.to_numpy()
 
-#%%
+# %%
 frequencies_masked = np.ma.masked_equal(frequencies, 0)
 min_f = np.amin(frequencies_masked, 0)
 max_f = np.amax(frequencies_masked, 0)
 
 chroma_range = max_f - min_f + 1
 
-print(min_f)
-print(max_f)
-print(chroma_range)
+print(f'min freq={min_f}')
+print(f'max freq={max_f}')
+print(f'chroma range={chroma_range}')
 # %%
 modulation = 8
 
 min_f_mod = min_f + modulation
 max_f_mod = max_f + modulation
 
-print(min_f_mod)
-print(max_f_mod)
+print(f'min_f_mod={min_f_mod}')
+print(f'max_f_mod={max_f_mod}')
 
 # %%
