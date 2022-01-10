2e86dc01c6c189bef779bd91f466960cb1e83c4b
JeroenOudeVrielink <jeroenov98@gmail.com>
2022-01-10 11:53:57 +0100

    adding analysing script

--- generate_train_vecs.py
+++ generate_train_vecs.py
@@ -2,23 +2,44 @@ from midi_to_5d_vec import midi_to_5d_vec
 import pandas as pd
 import numpy as np
 
-IGNORE_PAUSES = True
+ENCODE_PAUSES = True
+BIAS = True
+
 IMPORT_FILE = 'F.txt'
+OUTPUT_FILE = 'x'
+
+# Vector representation:
+# [music playing (0,1), pitch, chroma_x, chroma_y, circle5_x, circle5_y, log2(duration)] + (bias)
 
 
-def get_6d_vec(midi_value, duration) -> list:
+def get_pause_vector(duration) -> list:
+    """
+    Function that returns a vector of with 6 zero entries plus the log2(duration) and a possible bias
+    """
+    vec = [0] * 6
+    vec.append(np.log2(duration))
+    if BIAS:
+        vec.append(1)
+    return vec
+
+
+def get_vector(midi_value, duration) -> list:
     """
     Returns the midi value as a 5d vec representation plus
     an extra entry for the duration
     Duration is encoded as log2(duration)
-    [pitch, chroma_x, chroma_y, circle5_x, circle5_y, log2(duration)]
+    [music playing, pitch, chroma_x, chroma_y, circle5_x, circle5_y, log2(duration)] + bias
     """
-    vec = midi_to_5d_vec(midi_value)
+    vec = []
+    vec.append(1)  # Music playing
+    vec.extend(midi_to_5d_vec(midi_value))
     vec.append(np.log2(duration))
+    if BIAS:
+        vec.append(1)
     return vec
 
 
-def gen_6d_train_vecs(midi_values) -> list:
+def generate_training_vectors(midi_values) -> list:
     """
     Converts a sequence of midi values to a 6d representation
     """
@@ -30,12 +51,12 @@ def gen_6d_train_vecs(midi_values) -> list:
         if midi_values[i] == midi_values[i + 1]:
             duration += 1
         else:
-            # If one wants to ignore pauses in the voices
-            # @TODO how do we represent pauses?
-            if midi_values[i] == 0 and IGNORE_PAUSES:
-                pass
+            if midi_values[i] == 0:
+                if ENCODE_PAUSES:
+                    vec = get_pause_vector(7)
+                    train_vectors.append(vec)
             else:
-                vec = get_6d_vec(midi_values[i], duration)
+                vec = get_vector(midi_values[i], duration)
                 train_vectors.append(vec)
             # Reset duration to 1
             duration = 1
@@ -50,12 +71,12 @@ def main():
     # Only select first voice (for now) and discard last 16 midi notes
     midi_values = voices[0, :-16]
 
-    x = gen_6d_train_vecs(midi_values)
+    x = generate_training_vectors(midi_values)
     x = np.array(x)
 
     # Save as np array and as txt (for vizability)
-    np.save('data/x', x)
-    np.savetxt('data/x.txt', x, fmt='%s')
+    np.save(f'data/{OUTPUT_FILE}', x)
+    np.savetxt(f'data/{OUTPUT_FILE}.txt', x, fmt='%s')
 
 
 if __name__ == '__main__':
